1d53cb64e9b84f3ecfcb6d52647cb716fa77893b
Jose liza <joselizagaravito@gmail.com>
2022-06-23 16:29:45 -0500

    Actualizacion de carpetas

--- GalaxyTraining.Sales.Specification/Common/IoC.cs
+++ GalaxyTraining.Sales.Specification/Common/IoC.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using GalaxyTraining.Sales.Application.Interfaces;
+﻿using GalaxyTraining.Sales.Application.Interfaces;
 using GalaxyTraining.Sales.Common.Dates;
 using StructureMap;
 using StructureMap.Graph;
